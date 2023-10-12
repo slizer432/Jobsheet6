fa2300a0b08a46c09d9cda99fbc303e5232ed8a2
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-10-12 15:18:26 +0700

    update percobaan 1

--- pemilihan2Percobaan123.java
+++ pemilihan2Percobaan123.java
@@ -4,12 +4,18 @@ public class pemilihan2Percobaan123 {
         Scanner input23 = new Scanner(System.in);
         System.out.print("Masukkan Tahun : ");
         int tahun = input23.nextInt();
-        if (tahun % 4 == 0 ) {
-            if (tahun % 100 != 0)
+        if (tahun%4==0) {
+            if (tahun%100==0) {
+                if (tahun%400==0) {
+                    System.out.println("Tahun kabisat");
+                }else{
+                System.out.println("Tahun kabisat");
+                }
+            }else {
             System.out.println("Tahun kabisat");
-            else 
+        }
+        }else {
             System.out.println("Bukan tahun kabisat");
-        }else
-        System.out.println("Bukan tahun kabisat");
+        }
     }
 }
